acc87e1ba25f1f66e302f912dc756fff9e5b5deb
facundodev7 <facundoemanuel73@gmail.com>
2025-09-20 16:09:25 -0300

    Conexion a firebase

---
+++ src/database.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhA5C6gFzR8xV7K4vhcnIBHFV4QsreBfI",
+  authDomain: "patitas-6e697.firebaseapp.com",
+  projectId: "patitas-6e697",
+  storageBucket: "patitas-6e697.firebasestorage.app",
+  messagingSenderId: "630971918036",
+  appId: "1:630971918036:web:6385159245a6c9df8b7d41"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
