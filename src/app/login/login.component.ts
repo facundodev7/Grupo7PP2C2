c47dd593e9633b860935d13121aea7636696e23a
facundodev7 <facundoemanuel73@gmail.com>
2025-09-22 12:03:10 -0300

    login Funcional

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ControladorR } from '../../database';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  email:any
+  contra:any
+
+  form:any
+  formData = new FormData()
+
+  constructor(private controlador:ControladorR){}
+
+  subir(){
+    this.form = document.getElementById('formu')
+    this.formData = new FormData(this.form)
+
+    this.email = this.formData.get('usuario')
+    this.contra = this.formData.get('contra')
+
+    this.controlador.login(this.email,this.contra)
+  }
 
 }
