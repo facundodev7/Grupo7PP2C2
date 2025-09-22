a414c13cfb3f463fcd4f91080dc4aca0b7c97349
facundodev7 <facundoemanuel73@gmail.com>
2025-09-22 12:14:54 -0300

    u

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,50 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { IngresaComponent } from './ingresa/ingresa.component';
+import { TurnoComponent } from './turno/turno.component';
+import { RegistroComponent } from './registro/registro.component';
 
-const adminPage = {
-  path: 'login',
-  component: LoginComponent
-}
+const routeConfig: Routes = [
+  {
+    path: '',
+    component: InicioComponent,
+    title: 'Patitas Felices'
+  },
+  {
+    path:'login',
+    component:LoginComponent,
+    title:'Iniciar Sesion'
+  },
+  {
+    path:'regis',
+    component:RegistroComponent,
+    title:'Registrarse'
+  },
+  {
+    path:'quienes',
+    component:QuienesSomosComponent,
+    title:'Quienes Somos'
+  },
+  {
+    path:'contacto',
+    component:ContactoComponent,
+    title:'Contacto'
+  },
+  {
+    path:'ingresarM',
+    component:IngresaComponent,
+    title:'Ingresa tu Mascota'
+  },
+  {
+    path:'turno',
+    component:TurnoComponent,
+    title:'Saca un turno'
+  }
+];
 
-export const routes: Routes = [];
+export default routeConfig
+
